bde958999a231cab28e3de6dd1d94bb6d8d55573
shorif9 <91177901+shorif9@users.noreply.github.com>
2021-09-25 11:54:53 +0600

    Add files via upload

---
+++ firebaseConfiguration.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwN22F4veZw5fOJbDbVg72O7fzpKsh9DM",
+    authDomain: "auth--provider.firebaseapp.com",
+    databaseURL: "https://auth--provider-default-rtdb.firebaseio.com",
+    projectId: "auth--provider",
+    storageBucket: "auth--provider.appspot.com",
+    messagingSenderId: "808313755235",
+    appId: "1:808313755235:web:4609e388ca45e931af4a56"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
